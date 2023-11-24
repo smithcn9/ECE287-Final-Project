b83b06f3f9cf008bd6b36b11e7e58ae9dfcb0098
smithcn9 <150873409+smithcn9@users.noreply.github.com>
2023-11-23 23:27:05 -0500

    Update CoordinateSystemTB.v

--- CoordinateSystemTB.v
+++ CoordinateSystemTB.v
@@ -2,15 +2,12 @@
 
 module CoordinateSystemTB;
 
-	// Inputs to the CoordinateSystem module
 	reg clk;
 	reg rst;
 
-	// Outputs from the CoordinateSystem module
 	wire [9:0] x;
 	wire [9:0] y;
 
-	// Instantiate the CoordinateSystem module
 	CoordinateSystem uut (
     	.clk(clk),
     	.rst(rst),
@@ -18,23 +15,18 @@ module CoordinateSystemTB;
     	.y(y)
 	);
 
-	// Clock generation
 	initial begin
     	clk = 0;
-    	forever #10 clk = ~clk; // Clock with a period of 20ns (50MHz)
+    	forever #10 clk = ~clk;
 	end
 
-	// Test procedure
 	initial begin
-    	// Initialize Inputs
-    	rst = 1;   	// Assert reset
-    	#100;      	// Wait for 100ns
-    	rst = 0;   	// Deassert reset
+    	rst = 1;  
+    	#100;    
+    	rst = 0;  
 
-    	// Wait long enough to observe behavior
-    	#10000000;   	// Wait for 200,000ns (200us), adjust as needed
+    	#10000000; 
 
-    	// End simulation
     	$stop;
 	end
 
